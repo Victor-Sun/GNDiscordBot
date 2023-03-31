42fce0561c516a5d703d06096f9da222b3d54be8
Victor Sun <mynameisvictor@126.com>
2023-03-31 19:31:45 -0400

    Update

--- commands/moveSpam.js
+++ commands/moveSpam.js
@@ -1,4 +1,5 @@
 const {  SlashCommandBuilder } = require('@discordjs/builders')
+const BotSettings = require('../models/BotSettings')
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -15,32 +16,54 @@ module.exports = {
             .setRequired(true)
         ),
 	async execute(interaction) {
-		const channels = await interaction.member.guild.channels.fetch()
-        const victimId = interaction.options.getUser('user').id
-        let moveAmount = interaction.options.getInteger('amount')
-        
-        if (moveAmount > 10) {
-            moveAmount = 10
+        const moveSpam = await BotSettings.findOne({ name: 'moveSpam'})
+        const textChannel = interaction.channel
+        if (!moveSpam) {
+            await BotSettings.insertMany({name: 'moveSpam', value: true})
         }
 
-        const channelIds = []
-        for (const channel of channels.values()) {
-            if(channel.type === 'GUILD_VOICE') {
-                channelIds.push(channel.id)
+        if (moveSpam.value === false) {
+            interaction.reply('Command disabled')
+        } else {
+            const channels = await interaction.member.guild.channels.fetch()
+            const victimId = interaction.options.getUser('user').id
+            let moveAmount = interaction.options.getInteger('amount')
+            
+            if (moveAmount > 10) {
+                moveAmount = 10
             }
-        }
-
-        if (interaction.guild.members.cache.get(victimId).voice.channel) {
-            let moveCount = 0
-            while(moveCount < moveAmount) {
-                const randomChannel = Math.floor(Math.random() * channelIds.length)
-                if (randomChannel !== interaction.guild.members.cache.get(victimId).voice.channelId) {
-                    await interaction.guild.members.cache.get(victimId).voice.setChannel(channelIds[randomChannel])
-                    moveCount++
+    
+            const channelIds = []
+            for (const channel of channels.values()) {
+                if(channel.type === 'GUILD_VOICE') {
+                    channelIds.push(channel.id)
                 }
             }
+    
+            let moveSuccess = true
 
-            interaction.reply(`<@${victimId}> has been given aids.`)
+            if (interaction.guild.members.cache.get(victimId).voice.channel) {
+                let moveCount = 0
+                while(moveCount < moveAmount) {
+                    if (interaction.guild.members.cache.get(victimId).voice.channel) {
+                        const randomChannel = Math.floor(Math.random() * channelIds.length)
+                        if (randomChannel !== interaction.guild.members.cache.get(victimId).voice.channelId) {
+                            await interaction.guild.members.cache.get(victimId).voice.setChannel(channelIds[randomChannel])
+                            moveCount++
+                        }
+                    } else {
+                        interaction.reply(`<@${victimId}> is not connected to a voice channel.`)
+                        moveSuccess = false
+                        break
+                    }
+                }
+    
+                if (moveSuccess) {
+                    interaction.reply(`<@${victimId}> has been given aids.`)
+                }
+            } else {
+                interaction.reply(`<@${victimId}> is not connected to a voice channel.`)
+            }
         }
 	}
 }
