c6b970351335e98e99d16417f3876ccbe37a39d4
Victor Sun <mynameisvictor@126.com>
2026-06-14 20:52:43 -0400

    Add announce command

---
+++ commands/announce.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('announce')
+		.setDescription('Post an announcement in this channel')
+		.addStringOption(option => option
+			.setName('message')
+			.setDescription('The announcement message')
+			.setRequired(true))
+		.addStringOption(option => option
+			.setName('title')
+			.setDescription('Title for the announcement embed')
+			.setRequired(false)),
+	async execute(interaction) {
+		const title = interaction.options.getString('title') ?? 'Announcement';
+		const message = interaction.options.getString('message');
+
+		const embed = new MessageEmbed()
+			.setColor('#5865F2')
+			.setTitle(title)
+			.setDescription(message)
+			.setFooter({ text: `Announced by ${interaction.user.tag}` })
+			.setTimestamp();
+
+		await interaction.channel.send({ embeds: [embed] });
+		await interaction.reply({ content: 'Announcement posted.', ephemeral: true });
+	},
+};
